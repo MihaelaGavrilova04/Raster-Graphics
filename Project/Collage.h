ddd4486a95b6e597d6b642addb46175dfba217e7
MihaelaGavrilova04 <147726945+MihaelaGavrilova04@users.noreply.github.com>
2024-06-25 01:32:23 +0300

    Update Collage.h

--- Project/Collage.h
+++ Project/Collage.h
@@ -1,17 +1,17 @@
 #pragma once
 
 #include "Image.h"
-#include "SharedPtr.hpp"
-#include <iostream>
+#include <stdexcept>
+#include "myVector.hpp"
 
 class Collage {
 public:
     Collage() = default;
 
-    static SharedPtr<Image> horizontalCollage(const SharedPtr<Image>& image1, const SharedPtr<Image>& image2);
-    static SharedPtr<Image> verticalCollage(const SharedPtr<Image>& image1, const SharedPtr<Image>& image2);
+    static Image* horizontalCollage(Image* image1, Image* image2);
+    static Image* verticalCollage(Image* image1, Image* image2);
 
 private:
     // helper
-    static bool areCompatible(const SharedPtr<Image>& image1, const SharedPtr<Image>& image2);
+    static bool areCompatible(Image* image1, Image* image2);
 };
