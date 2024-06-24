c45cd8c8960dbcef88af4c5c6c2ec23fc305be06
MihaelaGavrilova04 <147726945+MihaelaGavrilova04@users.noreply.github.com>
2024-06-25 01:39:16 +0300

    Update CommandProcessor.cpp

--- Project - cpp files/CommandProcessor.cpp
+++ Project - cpp files/CommandProcessor.cpp
@@ -1,13 +1,15 @@
 #include "CommandProcessor.h"
 
-CommandProcessor::CommandProcessor(SessionHolder<numSessions>& holder):sessionHolder(holder) { };
+CommandProcessor::CommandProcessor(SessionHolder<numSessions>& holder): sessionHolder(holder) {}
 
-void CommandProcessor::processCommand(const myString& currentCommand) {
+void CommandProcessor::processCommand(const myString& currentCommand) 
+{
     std::stringstream ss(currentCommand.c_str());
     myString command;
     ss >> command;
 
-    if (command == "load") {
+    if (command == "load") 
+    {
         myString filename;
         ss >> filename;
 
@@ -15,133 +17,139 @@ void CommandProcessor::processCommand(const myString& currentCommand) {
         if (image) 
         {
             if (activeSessionId == -1) 
-            {
                 activeSessionId = sessionHolder.createSession();
-            }
+            
             sessionHolder.getSession(activeSessionId)->addImage(image);
-        }
+        } 
         else 
-            throw std::runtime_error("Can't load image from file!\n");
-        
-    }
+        throw std::runtime_error("Can't load image from file!\n");
+    } 
     else if (command == "close") 
     {
         int sessionId;
         ss >> sessionId;
         closeSession(sessionId);
         if (sessionId == activeSessionId)
-            activeSessionId = -1; 
-    }
-    else if (command == "save") 
+            activeSessionId = -1;
+
+    } 
+    else if (command == "save")
     {
         sessionHolder.saveAllSessions();
-    }
+
+    } 
     else if (command == "saveas") 
     {
         myString filename;
         ss >> filename;
         saveas(filename);
-    }
+    } 
     else if (command == "help") 
     {
         help();
-    }
-    else if (command == "grayscale") {
+    } 
+    else if (command == "grayscale") 
+    {
         grayscale(activeSessionId);
-    }
-    else if (command == "monochrome") {
+
+    } else if (command == "monochrome")
+    {
         monochrome(activeSessionId);
-    }
-    else if (command == "negative") {
+    } 
+    else if (command == "negative")
+    {
         negative(activeSessionId);
     }
-    else if (command == "rotate") {
+    else if (command == "rotate") 
+    {
         myString direction;
         ss >> direction;
         rotate(direction, activeSessionId);
-    }
-    else if (command == "undo") {
+    } 
+    else if (command == "undo") 
+    {
         undo();
     }
-    else if (command == "add") {
+    else if (command == "add") 
+    {
         myString filename;
         ss >> filename;
         add(activeSessionId, filename);
-    }
-    else if (command == "session") {
+    } 
+    else if (command == "session")
+    {
         sessionInfo(activeSessionId);
-    }
-    else if (command == "switch") {
+    } 
+    else if (command == "switch") 
+    {
         int sessionId;
         ss >> sessionId;
         switchSession(sessionId);
-        activeSessionId = sessionId; 
-    }
-    else if (command == "collage") {
+        activeSessionId = sessionId;
+    } 
+    else if (command == "collage") 
+    {
         myString direction, image1, image2, outImage;
         ss >> direction >> image1 >> image2 >> outImage;
         collage(direction, activeSessionId, image1, image2, outImage);
-    }
+    } 
     else if (command == "exit") 
     {
         std::exit(0);
-    }
+    } 
     else 
     {
-        throw std::logic_error("Invalid command\n " );
+        throw std::logic_error("Invalid command\n ");
     }
 }
 
-Image* CommandProcessor:: getImage(const char* filename)
+Image* CommandProcessor::getImage(const char* filename) 
 {
     Image* image = nullptr;
     myString file(filename);
     imageFormat format(file);
 
-    if (format.getExtension() == ".ppm")
+    if (format.getExtension() == ".ppm") 
     {
         PPM_Reader reader(filename);
         image = reader.getImage();
-    }
+    } 
     else if (format.getExtension() == ".pgm")
     {
         PGM_Reader reader(filename);
         image = reader.getImage();
-    }
-    else if (format.getExtension() == ".pbm")
-    {
+    } 
+    else if (format.getExtension() == ".pbm") {
         PBM_Reader reader(filename);
         image = reader.getImage();
     }
-    
+
     if (!image)
-        throw std::logic_error("Invalid extention!\n");
-    
+        throw std::logic_error("Invalid extension!\n");
 
     return image;
 }
 
-void CommandProcessor::closeSession(int sessionId)
+void CommandProcessor::closeSession(int sessionId) 
 {
     if (sessionHolder.sessionExists(sessionId))
         sessionHolder.removeSession(sessionId);
     else
         throw std::logic_error("No such sessionId\n");
 }
-void CommandProcessor::saveas(const myString& filename)const
+
+void CommandProcessor::saveas(const myString& filename) const 
 {
     if (sessionHolder.sessionExists(0))
     {
-        SharedPtr<Image> image = sessionHolder.getSession(0)->getImageAtIndex(0);
+        Image* image = sessionHolder.getSession(0)->getImageAtIndex(0);
         sessionHolder.getSession(0)->saveImage(image, filename);
     }
     else
-    throw std::logic_error("No image available!\n");
-    
+        throw std::logic_error("No image available!\n");
 }
 
-void CommandProcessor::help()
-{
+void CommandProcessor::help() {
     std::cout << "-Available commands-\n";
     std::cout << "  load <filename> - Loads an image into new session\n";
     std::cout << "  close <sessionId> - Closes the session with sessionID\n";
@@ -158,87 +166,99 @@ void CommandProcessor::help()
     std::cout << "  collage <direction> <sessionId> <firstImage> <secondImage> <outputImage> - Creates collage\n";
     std::cout << "  exit - Exit the application\n";
 }
-void CommandProcessor::grayscale(int sessionId) {
-    if (sessionHolder.sessionExists(sessionId)) {
+
+void CommandProcessor::grayscale(int sessionId) 
+{
+    if (sessionHolder.sessionExists(sessionId)) 
+    {
         Session<sessionCapacity>* session = sessionHolder.getSession(sessionId);
         int imagesInSession = session->getNumImages();
         GrayscaleConverter grayscaleConverter;
 
-            SharedPtr<ImageCommand> command(new GrayscaleCommand(&grayscaleConverter));
-            session->addTransformation(command); //in add we also apply the transformations
-    }
-    else 
-        throw std::logic_error("Session doesn't exist!\n");
+        ImageCommand* command = new GrayscaleCommand(&grayscaleConverter);
+        session->addTransformation(command);
+    } 
+    else
+    throw std::logic_error("Session doesn't exist!\n");
 }
 
-void CommandProcessor::monochrome(int sessionId) {
-    if (sessionHolder.sessionExists(sessionId)) {
+void CommandProcessor::monochrome(int sessionId) 
+{
+    if (sessionHolder.sessionExists(sessionId)) 
+    {
         Session<sessionCapacity>* session = sessionHolder.getSession(sessionId);
         int imagesInSession = session->getNumImages();
         MonochromeConverter monochromeConverter;
 
-            SharedPtr<ImageCommand> command(new MonochromeCommand(&monochromeConverter));
-            session->addTransformation(command);
-    }
-    else 
-        throw std::logic_error("Session does't exist!\n");
+        ImageCommand* command = new MonochromeCommand(&monochromeConverter);
+        session->addTransformation(command);
+    } 
+    else
+    throw std::logic_error("Session doesn't exist!\n");
 }
-void CommandProcessor::negative(int sessionId)
+
+void CommandProcessor::negative(int sessionId) 
 {
-    if (sessionHolder.sessionExists(sessionId)) {
+    if (sessionHolder.sessionExists(sessionId)) 
+    {
         Session<sessionCapacity>* session = sessionHolder.getSession(sessionId);
         int imagesInSession = session->getNumImages();
         NegativeConverter negativeConverter;
 
-            SharedPtr<ImageCommand> command(new NegativeCommand(&negativeConverter));
-            session->addTransformation(command);
-    }
+        ImageCommand* command = new NegativeCommand(&negativeConverter);
+        session->addTransformation(command);
+    } 
     else
-        throw std::logic_error("Session does't exist!\n");
+    throw std::logic_error("Session doesn't exist!\n");
 }
-void CommandProcessor::rotate(const myString& direction, int sessionId)
-{
-    if (sessionHolder.sessionExists(sessionId)) {
+
+void CommandProcessor::rotate(const myString& direction, int sessionId) {
+    if (sessionHolder.sessionExists(sessionId)) 
+    {
         Session<sessionCapacity>* session = sessionHolder.getSession(sessionId);
         int imagesInSession = session->getNumImages();
         Rotator rotator;
-        SharedPtr<ImageCommand> command(new RotateCommand(&rotator, direction));
+
+        ImageCommand* command = new RotateCommand(&rotator, direction);
         session->addTransformation(command);
-    }
-    else 
-        throw std::logic_error("Session does't exist!\n");
+    } 
+    else
+    throw std::logic_error("Session doesn't exist!\n");
 }
+
 void CommandProcessor::undo()
 {
-        sessionHolder.getSession(activeSessionId)->undoLastTransformation();
+    sessionHolder.getSession(activeSessionId)->undoLastTransformation();
 }
 
-void CommandProcessor::add(int sessionId, const myString& filename) {
-    if (sessionHolder.sessionExists(sessionId)) {
-        Image* image = getImage(filename.c_str());
-        if (image) {
-            sessionHolder.getSession(sessionId)->addImage(image);
-        }
-        else {
-            throw std::runtime_error("Can't load image from file!\n");
-        }
-    }
-    else 
-        throw std::logic_error("Session doesn't exist!\n");
-    
-}
-
-void CommandProcessor::sessionInfo(int sessionId) const {
+void CommandProcessor::add(int sessionId, const myString& filename) 
+{
     if (sessionHolder.sessionExists(sessionId)) 
+    {
+        Image* image = getImage(filename.c_str());
+        if (image) 
+            sessionHolder.getSession(sessionId)->addImage(image);
+        else 
+        throw std::runtime_error("Can't load image from file!\n");
+       
+    }   
+    else
+    throw std::logic_error("Session doesn't exist!\n");
+}
+
+void CommandProcessor::sessionInfo(int sessionId) const 
+{
+    if (sessionHolder.sessionExists(sessionId))
         sessionHolder.getSession(sessionId)->sessionInfo();
     else
         throw std::logic_error("Session doesn't exist!\n");
 }
 
-void CommandProcessor::switchSession(int sessionId) {
-    if (sessionHolder.sessionExists(sessionId)) 
+void CommandProcessor::switchSession(int sessionId) 
+{
+    if (sessionHolder.sessionExists(sessionId))
         sessionHolder.setCurrentSession(sessionId);
-    else 
+    else
         throw std::logic_error("Session doesn't exist!\n");
 }
 
@@ -251,23 +271,21 @@ void CommandProcessor::collage(const myString& dir, int sesId, const myString& f
 
         if (img1 && img2)
         {
-            SharedPtr<Image> collage;
+            Image* collage = nullptr;
             if (dir == "horizontal")
             {
-                collage = Collage::horizontalCollage(SharedPtr<Image>(img1), SharedPtr<Image>(img2));
+                collage = Collage::horizontalCollage(img1, img2);
             }
             else if (dir == "vertical")
             {
-                collage = Collage::verticalCollage(SharedPtr<Image>(img1), SharedPtr<Image>(img2));
+                collage = Collage::verticalCollage(img1, img2);
             }
             else
             {
-                throw std::logic_error("Invald direction!\n");
+                throw std::logic_error("Invalid direction!\n");
             }
 
-            myString collageFilename(OutputFile);
-
-            sessionHolder.getSession(activeSessionId)->saveImage(collage, collageFilename);
+            sessionHolder.getSession(activeSessionId)->saveImage(collage, OutputFile);
         }
         else
             throw std::runtime_error("Can't create collage!\n");
@@ -275,3 +293,4 @@ void CommandProcessor::collage(const myString& dir, int sesId, const myString& f
     else
         throw std::logic_error("Session doesn't exist!\n");
 }
+
